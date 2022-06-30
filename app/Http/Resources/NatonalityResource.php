2931e11501e7aa167ca43f7f3694ea6828e5c00a
islamshu12 <islamshu12@gmail.com>
2022-06-30 10:39:53 +0300

    ي

--- app/Http/Resources/NatonalityResource.php
+++ app/Http/Resources/NatonalityResource.php
@@ -18,10 +18,7 @@ class NatonalityResource extends JsonResource
         return [
           'id'=>$this->id,
           'name'=>$this->name,
-          'flag'=>asset('uploads/'.$this->flag),  
-          // 'workers'=>WorkerResource::collection($this->worker)
-          // 'wrokers'=> WorkerResource::collection(
-            // Worker::has('company')->get()),
+          
         ];
     }
 }
