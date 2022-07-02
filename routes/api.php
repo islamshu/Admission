c7b5d6265e9d4430ce6005106b153f74429f49f6
islamshu12 <islamshu12@gmail.com>
2022-07-02 18:55:03 +0300

    f

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+Route::group(['middleware' => [ 'changeLanguage']], function () {
 Route::get('natonality','Api\HomeController@natonality');
 Route::get('worker_by_natonality','Api\HomeController@worker_by_natonality');
 Route::get('copmanies','Api\HomeController@compnaines');
@@ -31,5 +32,5 @@ Route::get('FAQs','Api\HomeController@faqs');
 Route::get('general','Api\HomeController@general');
 Route::post('request_worker','Api\HomeController@request_worker');
 Route::post('contact_form','Api\HomeController@contact_form');
-
+});
 
