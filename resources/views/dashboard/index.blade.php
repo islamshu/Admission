8d890a674225359a3163f05d8716148ff8de8721
islamshu12 <islamshu12@gmail.com>
2022-07-03 19:21:50 +0300

    ي

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -150,6 +150,69 @@
           </div>
         </div>
       </div>
+      <div class="col-xl-3 col-lg-6 col-12">
+        <div class="card pull-up">
+          <div class="card-content">
+            <div class="card-body">
+              <div class="media d-flex">
+                <div class="media-body text-left">
+                  <h3 class="success">{{ App\Booking::where('status',1)->count() }}</h3>
+                  <h6>{{ __('Done order') }}</h6>
+                </div>
+                <div>
+                  <i class="icon-user-following success font-large-2 float-right"></i>
+                </div>
+              </div>
+              <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+                <div class="progress-bar bg-gradient-x-success" role="progressbar" style="width: 65%"
+                aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-lg-6 col-12">
+        <div class="card pull-up">
+          <div class="card-content">
+            <div class="card-body">
+              <div class="media d-flex">
+                <div class="media-body text-left">
+                  <h3 class="danger">{{ App\Booking::where('status',0)->count() }}</h3>
+                  <h6>{{ __('Reject order') }}</h6>
+                </div>
+                <div>
+                  <i class="icon-user-following danger font-large-2 float-right"></i>
+                </div>
+              </div>
+              <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+                <div class="progress-bar bg-gradient-x-danger" role="progressbar" style="width: 65%"
+                aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-lg-6 col-12">
+        <div class="card pull-up">
+          <div class="card-content">
+            <div class="card-body">
+              <div class="media d-flex">
+                <div class="media-body text-left">
+                  <h3 class="warning">{{ App\Booking::where('status',2)->count() }}</h3>
+                  <h6>{{ __('in progress order') }}</h6>
+                </div>
+                <div>
+                  <i class="icon-user-following warning font-large-2 float-right"></i>
+                </div>
+              </div>
+              <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+                <div class="progress-bar bg-gradient-x-warning" role="progressbar" style="width: 65%"
+                aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
      
    
       
