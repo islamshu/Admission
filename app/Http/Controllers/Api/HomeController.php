a861174a911712bc055ad7c13f1c2068ff0fb6e9
islamshu12 <islamshu12@gmail.com>
2022-07-03 18:03:07 +0300

    f

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -224,7 +224,9 @@ class HomeController extends BaseController
         return WorkerResource::collection($camp);
     }
     public function count_vist(){
-        $general = General::where('key','visitor')->first()->value;
-        dd($general);
+        $general = General::where('key','visitor')->first();
+        $general->value +=1;
+        $general->save();
+        return $general;
     }
 }
