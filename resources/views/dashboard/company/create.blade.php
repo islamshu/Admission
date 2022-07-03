155947c318080855393d7a011b13f3930f178ad7
islamshu12 <islamshu12@gmail.com>
2022-07-03 15:08:04 +0300

    d

--- resources/views/dashboard/company/create.blade.php
+++ resources/views/dashboard/company/create.blade.php
@@ -260,19 +260,17 @@
                     google.maps.event.addListener(marker, 'dragend', function(evt){
                         console.log(this.getPosition());
                         setLocationCoordinates(autocomplete.key, this.getPosition().lat(), this.getPosition().lng());
-                        var latlng = new google.maps.LatLng(this.getPosition().lat(), this.getPosition().lng());
-                    // This is making the Geocode request
-                    var geocoder = new google.maps.Geocoder();
-                    geocoder.geocode({ 'latLng': latlng },  (results, status) =>{
-                        if (status !== google.maps.GeocoderStatus.OK) {
-                            alert(status);
-                        }
-                        // This is checking to see if the Geoeode Status is OK before proceeding
-                        if (status == google.maps.GeocoderStatus.OK) {
-                            console.log(results);
-                            var address = (results[0].formatted_address);
-                        }
-                    });
+                        var lat = parseFloat(document.getElementById("txtLatitude").value);
+                        var lng = parseFloat(document.getElementById("txtLongitude").value);
+                        var latlng = new google.maps.LatLng(lat, lng);
+                        var geocoder = geocoder = new google.maps.Geocoder();
+                        geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+                            if (status == google.maps.GeocoderStatus.OK) {
+                                if (results[1]) {
+                                    alert("Location: " + results[1].formatted_address);
+                                }
+                            }
+                        });
 
                         console.log(this.getPosition().lat());
                     });
