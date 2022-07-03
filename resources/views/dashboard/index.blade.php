bd714fab2d787dd4e398f2306c3f6dcf1cfcdda2
islamshu12 <islamshu12@gmail.com>
2022-07-03 17:08:13 +0300

    ب

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -67,6 +67,69 @@
       </div>
     </div>
     <div class="col-xl-3 col-lg-6 col-12">
+      <div class="card pull-up">
+        <div class="card-content">
+          <div class="card-body">
+            <div class="media d-flex">
+              <div class="media-body text-left">
+                <h3 class="success">{{ App\Worker::where('status',1)->count() }}</h3>
+                <h6>{{ __('available Worker Count') }}</h6>
+              </div>
+              <div>
+                <i class="icon-loop  success font-large-2 float-right"></i>
+              </div>
+            </div>
+            <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+              <div class="progress-bar bg-gradient-x-success" role="progressbar" style="width: 75%"
+              aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-3 col-lg-6 col-12">
+      <div class="card pull-up">
+        <div class="card-content">
+          <div class="card-body">
+            <div class="media d-flex">
+              <div class="media-body text-left">
+                <h3 class="danger">{{ App\Worker::where('status',0)->count() }}</h3>
+                <h6>{{ __('Busy Worker Count') }}</h6>
+              </div>
+              <div>
+                <i class="icon-loop  danger font-large-2 float-right"></i>
+              </div>
+            </div>
+            <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+              <div class="progress-bar bg-gradient-x-danger" role="progressbar" style="width: 75%"
+              aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-3 col-lg-6 col-12">
+      <div class="card pull-up">
+        <div class="card-content">
+          <div class="card-body">
+            <div class="media d-flex">
+              <div class="media-body text-left">
+                <h3 class="dark">{{ App\Worker::where('status',2)->count() }}</h3>
+                <h6>{{ __('In Progress Worker Count') }}</h6>
+              </div>
+              <div>
+                <i class="icon-loop  dark font-large-2 float-right"></i>
+              </div>
+            </div>
+            <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+              <div class="progress-bar bg-gradient-x-dark" role="progressbar" style="width: 75%"
+              aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-3 col-lg-6 col-12">
         <div class="card pull-up">
           <div class="card-content">
             <div class="card-body">
