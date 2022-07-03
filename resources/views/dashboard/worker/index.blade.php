0a6c3c4013693ab1346994e6996c0cfdd037012b
islamshu12 <islamshu12@gmail.com>
2022-07-03 19:36:14 +0300

    k

--- resources/views/dashboard/worker/index.blade.php
+++ resources/views/dashboard/worker/index.blade.php
@@ -189,7 +189,7 @@
 @section('script')
     <script>
             function myFunction(id){
-                alert($id)
+                alert(id);
                 let status = $('#worker_status_'+id).val();
             let worker_id = id;
             $.ajax({
