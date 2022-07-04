6b95b51797f9cb56cf8f3bdaa609564d995d2f8e
islamshu12 <islamshu12@gmail.com>
2022-07-04 12:25:28 +0300

    d

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -246,189 +246,201 @@
 
     </div>
 @elseif(auth()->user()->hasRole('Company'))
-    <div class="col-xl-3 col-lg-6 col-12">
-        <div class="card pull-up">
-            <div class="card-content">
-                <div class="card-body">
-                    <div class="media d-flex">
-                        <div class="media-body text-left">
-                            <h3 class="info">
-                                {{ App\Worker::where('company_id', auth()->user()->company->id)->count() }}</h3>
-                            <h6>{{ __('Worker Count') }}</h6>
-                        </div>
-                        <div>
-                            <i class="icon-loop  info font-large-2 float-right"></i>
-                        </div>
-                    </div>
-                    <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-                        <div class="progress-bar bg-gradient-x-info" role="progressbar" style="width: 75%"
-                            aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-lg-6 col-12">
-        <div class="card pull-up">
-            <div class="card-content">
-                <div class="card-body">
-                    <div class="media d-flex">
-                        <div class="media-body text-left">
-                            <h3 class="success">
-                                {{ App\Worker::where('company_id', auth()->user()->company->id)->where('status', 1)->count() }}
-                            </h3>
-                            <h6>{{ __('available Worker Count') }}</h6>
-                        </div>
-                        <div>
-                            <i class="icon-loop  success font-large-2 float-right"></i>
-                        </div>
-                    </div>
-                    <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-                        <div class="progress-bar bg-gradient-x-success" role="progressbar" style="width: 75%"
-                            aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-lg-6 col-12">
-        <div class="card pull-up">
-            <div class="card-content">
-                <div class="card-body">
-                    <div class="media d-flex">
-                        <div class="media-body text-left">
-                            <h3 class="danger">
-                                {{ App\Worker::where('company_id', auth()->user()->company->id)->where('status', 0)->count() }}
-                            </h3>
-                            <h6>{{ __('Busy Worker Count') }}</h6>
-                        </div>
-                        <div>
-                            <i class="icon-loop  danger font-large-2 float-right"></i>
-                        </div>
-                    </div>
-                    <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-                        <div class="progress-bar bg-gradient-x-danger" role="progressbar" style="width: 75%"
-                            aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-lg-6 col-12">
-        <div class="card pull-up">
-            <div class="card-content">
-                <div class="card-body">
-                    <div class="media d-flex">
-                        <div class="media-body text-left">
-                            <h3 class="warning">
-                                {{ App\Worker::where('company_id', auth()->user()->company->id)->where('status', 2)->count() }}
-                            </h3>
-                            <h6>{{ __('In Progress Worker Count') }}</h6>
-                        </div>
-                        <div>
-                            <i class="icon-loop  warning font-large-2 float-right"></i>
-                        </div>
-                    </div>
-                    <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-                        <div class="progress-bar bg-gradient-x-warning" role="progressbar" style="width: 75%"
-                            aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-lg-6 col-12">
-        <div class="card pull-up">
-            <div class="card-content">
-                <div class="card-body">
-                    <div class="media d-flex">
-                        <div class="media-body text-left">
-                            <h3 class="dark">
-                                {{ App\Booking::where('company_id', auth()->user()->company->id)->count() }}</h3>
-                            <h6>{{ __('Number Of order') }}</h6>
-                        </div>
-                        <div>
-                            <i class="icon-user-following dark font-large-2 float-right"></i>
-                        </div>
-                    </div>
-                    <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-                        <div class="progress-bar bg-gradient-x-dark" role="progressbar" style="width: 65%"
-                            aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-xl-3 col-lg-6 col-12">
-        <div class="card pull-up">
-            <div class="card-content">
-                <div class="card-body">
-                    <div class="media d-flex">
-                        <div class="media-body text-left">
-                            <h3 class="success">
-                                {{ App\Booking::where('status', 1)->where('company_id', auth()->user()->company->id)->count() }}
-                            </h3>
-                            <h6>{{ __('Done Order') }} </h6>
-                        </div>
-                        <div>
-                            <i class="icon-user-following success font-large-2 float-right"></i>
-                        </div>
-                    </div>
-                    <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-                        <div class="progress-bar bg-gradient-x-success" role="progressbar" style="width: 65%"
-                            aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-lg-6 col-12">
-        <div class="card pull-up">
-            <div class="card-content">
-                <div class="card-body">
-                    <div class="media d-flex">
-                        <div class="media-body text-left">
-                            <h3 class="danger">
-                                {{ App\Booking::where('status', 0)->where('company_id', auth()->user()->company->id)->count() }}
-                            </h3>
-                            <h6>{{ __('Reject Order') }} </h6>
-                        </div>
-                        <div>
-                            <i class="icon-user-following danger font-large-2 float-right"></i>
-                        </div>
-                    </div>
-                    <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-                        <div class="progress-bar bg-gradient-x-danger" role="progressbar" style="width: 65%"
-                            aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-lg-6 col-12">
-        <div class="card pull-up">
-            <div class="card-content">
-                <div class="card-body">
-                    <div class="media d-flex">
-                        <div class="media-body text-left">
-                            <h3 class="warning">
-                                {{ App\Booking::where('status', 2)->where('company_id', auth()->user()->company->id)->count() }}
-                            </h3>
-                            <h6>{{ __('in progress order Order') }} </h6>
-                        </div>
-                        <div>
-                            <i class="icon-user-following warning font-large-2 float-right"></i>
-                        </div>
-                    </div>
-                    <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-                        <div class="progress-bar bg-gradient-x-warning" role="progressbar" style="width: 65%"
-                            aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="col-xl-3 col-lg-6 col-12">
+  <div class="card pull-up">
+      <div class="card-content">
+          <div class="card-body">
+              <div class="media d-flex">
+                  
+                  <a href="" data-toggle="modal" data-target="#all_worker">
+                      <div class="media-body text-left">
+                          <h3 class="info">{{ App\Worker::where('copmany_id',auth()->user()->company->id)->count() }}</h3>
+                          <h6>{{ __('Worker Count') }}</h6>
+                      </div>
+                  </a>
+                  <div>
+                      <i class="icon-loop  info font-large-2 float-right"></i>
+                  </div>
+              </div>
+              <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+                  <div class="progress-bar bg-gradient-x-info" role="progressbar" style="width: 75%"
+                      aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+<div class="col-xl-3 col-lg-6 col-12">
+  <div class="card pull-up">
+      <div class="card-content">
+          <div class="card-body">
+              <div class="media d-flex">
+                  
+                  <a href="" data-toggle="modal" data-target="#all_worker_avalable">
+                    <div class="media-body text-left">
+                      <h3 class="success">{{ App\Worker::where('copmany_id',auth()->user()->company->id)->where('status', 1)->count() }}</h3>
+                      <h6>{{ __('available Worker Count') }}</h6>
+                  </div>
+                </a>
+                  <div>
+                      <i class="icon-loop  success font-large-2 float-right"></i>
+                  </div>
+              </div>
+              <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+                  <div class="progress-bar bg-gradient-x-success" role="progressbar" style="width: 75%"
+                      aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+<div class="col-xl-3 col-lg-6 col-12">
+  <div class="card pull-up">
+      <div class="card-content">
+          <div class="card-body">
+              <div class="media d-flex">
+                  
+                  <a href="" data-toggle="modal" data-target="#all_worker_busy">
+                    <div class="media-body text-left">
+                      <h3 class="danger">{{ App\Worker::where('copmany_id',auth()->user()->company->id)->where('status', 0)->count() }}</h3>
+                      <h6>{{ __('Busy Worker Count') }}</h6>
+                  </div>
+                </a>
+                  <div>
+                      <i class="icon-loop  danger font-large-2 float-right"></i>
+                  </div>
+              </div>
+              <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+                  <div class="progress-bar bg-gradient-x-danger" role="progressbar" style="width: 75%"
+                      aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+<div class="col-xl-3 col-lg-6 col-12">
+  <div class="card pull-up">
+      <div class="card-content">
+          <div class="card-body">
+              <div class="media d-flex">
+                  
+                  <a href="" data-toggle="modal" data-target="#all_worker_progress">
+                    <div class="media-body text-left">
+                      <h3 class="dark">{{ App\Worker::where('copmany_id',auth()->user()->company->id)->where('status', 2)->count() }}</h3>
+                      <h6>{{ __('In Progress Worker Count') }}</h6>
+                  </div>
+                </a>
+                  <div>
+                      <i class="icon-loop  dark font-large-2 float-right"></i>
+                  </div>
+              </div>
+              <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+                  <div class="progress-bar bg-gradient-x-dark" role="progressbar" style="width: 75%"
+                      aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+<div class="col-xl-3 col-lg-6 col-12">
+  <div class="card pull-up">
+      <div class="card-content">
+          <div class="card-body">
+              <div class="media d-flex">
+                  
+                      
+                  <a href="" data-toggle="modal" data-target="#all_booking">
+                    <div class="media-body text-left">
+                      <h3 class="warning">{{ App\Booking::where('copmany_id',auth()->user()->company->id)->count() }}</h3>
+                      <h6>{{ __('Number Of order') }}</h6>
+                  </div>
+                </a>
+                  
+                  <div>
+                      <i class="icon-user-following warning font-large-2 float-right"></i>
+                  </div>
+              </div>
+              <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+                  <div class="progress-bar bg-gradient-x-warning" role="progressbar" style="width: 65%"
+                      aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+<div class="col-xl-3 col-lg-6 col-12">
+  <div class="card pull-up">
+      <div class="card-content">
+          <div class="card-body">
+              <div class="media d-flex">
+                 
+                  <a href="" data-toggle="modal" data-target="#all_booking_avalable">
+                    <div class="media-body text-left">
+                      <h3 class="success">{{ App\Booking::where('copmany_id',auth()->user()->company->id)->where('status', 1)->count() }}</h3>
+                      <h6>{{ __('Done Order') }} </h6>
+                  </div>
+                </a>
+                  <div>
+                      <i class="icon-user-following success font-large-2 float-right"></i>
+                  </div>
+              </div>
+              <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+                  <div class="progress-bar bg-gradient-x-success" role="progressbar" style="width: 65%"
+                      aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+<div class="col-xl-3 col-lg-6 col-12">
+  <div class="card pull-up">
+      <div class="card-content">
+          <div class="card-body">
+              <div class="media d-flex">
+                 
+                  <a href="" data-toggle="modal" data-target="#all_booking_busy">
+                    <div class="media-body text-left">
+                      <h3 class="danger">{{ App\Booking::where('copmany_id',auth()->user()->company->id)->where('status', 0)->count() }}</h3>
+                      <h6>{{ __('Reject Order') }} </h6>
+                  </div>
+                </a>
+                  <div>
+                      <i class="icon-user-following danger font-large-2 float-right"></i>
+                  </div>
+              </div>
+              <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+                  <div class="progress-bar bg-gradient-x-danger" role="progressbar" style="width: 65%"
+                      aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+<div class="col-xl-3 col-lg-6 col-12">
+  <div class="card pull-up">
+      <div class="card-content">
+          <div class="card-body">
+              <div class="media d-flex">
+                  
+                  <a href="" data-toggle="modal" data-target="#all_booking_progress">
+                    <div class="media-body text-left">
+                      <h3 class="warning">{{ App\Booking::where('copmany_id',auth()->user()->company->id)->where('status', 2)->count() }}</h3>
+                      <h6>{{ __('in progress order Order') }} </h6>
+                  </div>
+                </a>
+                  
+                  <div>
+                      <i class="icon-user-following warning font-large-2 float-right"></i>
+                  </div>
+              </div>
+              <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+                  <div class="progress-bar bg-gradient-x-warning" role="progressbar" style="width: 65%"
+                      aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
 
 
 
