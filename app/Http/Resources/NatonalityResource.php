7527adf14d100abd4eb9e85e388158e8d9b20785
islamshu12 <islamshu12@gmail.com>
2022-07-05 13:47:53 +0300

    d

--- app/Http/Resources/NatonalityResource.php
+++ app/Http/Resources/NatonalityResource.php
@@ -15,6 +15,7 @@ class NatonalityResource extends JsonResource
      */
     public function toArray($request)
     {
+        dd($request);
         return [
           'id'=>$this->id,
           'name'=>$this->name,
