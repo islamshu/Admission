a5e98f970b11368af7be976f72a3ea952271f608
islamshu12 <islamshu12@gmail.com>
2022-07-06 18:48:51 +0300

    ي

--- app/Http/Resources/WorkerResource.php
+++ app/Http/Resources/WorkerResource.php
@@ -19,7 +19,7 @@ class WorkerResource extends JsonResource
             'id'=>$this->id,
             'name'=>$this->name,
             'image'=>asset('uploads/'.$this->image),
-            'video'=>$this->video != null ? asset('uploads/'.$this->video) : null,
+            'video'=>$this->video != null ? $this->video : null,
             'natonality'=>new NatonalityResourceWithoutWorker(@$this->natonality),
             'age'=>$this->age,
             'experience'=>$this->experience,
