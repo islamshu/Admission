d5cbb9985d9a7f1951fccb1fd2a335a04807e7eb
islamshu12 <islamshu12@gmail.com>
2022-07-19 11:14:50 +0300

    ب

--- resources/views/dashboard/users/edit_client.blade.php
+++ resources/views/dashboard/users/edit_client.blade.php
@@ -72,7 +72,7 @@ input[type=submit] {
                 <div class="col-md-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4 class="card-title" id="basic-layout-colored-form-control">@lang('Create Client')  </h4>
+                            <h4 class="card-title" id="basic-layout-colored-form-control">@lang('Edit Client')  </h4>
                             <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
                             <div class="heading-elements">
                                 <ul class="list-inline mb-0">
