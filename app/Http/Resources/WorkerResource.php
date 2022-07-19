b502ad95ea3daf490d499683444ee1ea3dd1b800
islamshu12 <islamshu12@gmail.com>
2022-07-19 17:14:58 +0300

    d

--- app/Http/Resources/WorkerResource.php
+++ app/Http/Resources/WorkerResource.php
@@ -53,14 +53,15 @@ class WorkerResource extends JsonResource
         
         $last_booking = Booking::where('user_id',auth('client_api')->id())->where('worker_id',$data->id)->orderBy('id', 'DESC')->first();
         $worker = Worker::find($data->id);
+        $status=worker_status_id($data->id);
         if($last_booking){
-        if($worker->status == 1 && $last_booking->status == 0 ){
+        if(($status == 1 || $status==2) && $last_booking->status == 0 ){
             return 1;
         }else{
             return 0;
         }
      }else{
-        if($worker->status == 1 ){
+        if($status == 1 ){
             return 1;
         }else{
             return 0;
