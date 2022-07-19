56342f0c4b34c7a3a33aac0c1d2e8911af40a3ac
islamshu12 <islamshu12@gmail.com>
2022-07-19 20:39:18 +0300

    d

--- app/Http/Resources/BookingResoure.php
+++ app/Http/Resources/BookingResoure.php
@@ -26,7 +26,7 @@ class BookingResoure extends JsonResource
             'id_number'=>$this->id_number,
             'DOB'=>$this->DOB,
             'phone'=>$this->phone,
-            'status'=>$this->get_status($this),
+            'booking_status'=>$this->get_status($this),
             'visa_number'=>$this->visa_number,
             'visa_image'=>asset('uploads/'.$this->visa_image),
             'created_at'=>$this->created_at->format('Y-m-d')
