a7fa1b8f86ba0894f39523b172e03a5ed6bae1d9
islamshu12 <islamshu12@gmail.com>
2022-07-23 11:54:49 +0300

    ي

--- resources/views/dashboard/booking/pdf.blade.php
+++ resources/views/dashboard/booking/pdf.blade.php
@@ -3,53 +3,94 @@
 
 <head>
     <style>
-        *{
-            text-align: center
+        * {
+            direction: rtl
         }
+
+        table {
+            width: 100%;
+        }
+
         table,
         th,
         td {
-            border: 1px solid;
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        th,
+        td {
+            padding: 15px;
+            text-align: left;
+        }
+
+        table#t01 tr:nth-child(even) {
+            background-color: #eee;
+        }
+
+        table#t01 tr:nth-child(odd) {
+            background-color: #fff;
+        }
+
+        table#t01 th {
+            background-color: black;
+            color: white;
+        }
+
+        .center {
+            text-align: center
         }
     </style>
 </head>
 
 <body>
-    <h5>الحجوزات</h5>
 
-    <table class="container" style="width: 100%;">
+    <h2 class="center"> <img
+            src="https://workers.foryougo.net/uploads/general/PwsoWpkahkzTTiLtkFQRKzwOLPCKs1Uo7BTwjtt3.jpg" width="150"
+            height="100" /></h2>
+    <h2 class="center">الحجوزات</h2>
+
+    <table id="t01">
         <thead>
             <tr>
+                
+               
+                
+               
+               
+                
+                
+
+
+
                 <th>
-                    <h5>رقم الطلب</h5>
-                </th>
-<th>
-                    <h5>اسم العميل </h5>
-                </th>
-<th>
-                    <h5>اسم العاملة</h5>
-                </th>
-<th>
-                    <h5>رقم الهاتف</h5>
+                    رقم الجواز او الاقامة
                 </th>
                 <th>
-                    <h5>{{ __('Created at') }} </h5>
-                </th>      
-<th>
-                    <h5>تاريخ الميلاد</h5>
+                    رقم الهوية الخاص بالعميل
                 </th>
-<th>
-                    <h5>الحالة</h5>
+                <th>
+                    الحالة
                 </th>
-<th>
-                    <h5>رقم الهوية الخاص بالعميل</h5>
+                <th>
+                    تاريخ الميلاد
                 </th>
-<th>
-                    <h5>رقم الجواز او الاقامة</h5>
+                <th>
+                    {{ __('Created at') }} 
                 </th>
-{{-- <th>
-                    <h5>صورة الفيزا</h5>
-                </th> --}}
+                <th>
+                    رقم الهاتف
+                </th>
+                <th>
+                    اسم العاملة
+                </th>
+                <th>
+                    اسم العميل 
+                </th>
+                <th>
+                    رقم الطلب
+                </th>
+                
 
 
             </tr>
@@ -58,17 +99,17 @@
 
             @foreach ($bookings as $booking)
                 <tr>
-                    <td>{{ $booking->order_id }}</td>
-                    <td>{{   $booking->name}}</td>
-                    <td>{{  $booking->worker->name }}</td>
-                    <td> {{  $booking->phone }}</td>
-                    <td> {{$booking->DOB }}</td>
-                    <td>{{$booking->created_at->format('Y-m-d') }}</td>
-                    <td> {{ booking_status($booking->status) }}</td>
-                    <td>{{  $booking->id_number }}</td>
+
 
                     <td> {{ $booking->visa_number }}</td>
-                    {{-- <td>{{  asset('uploads/'.$booking->visa_image) }}</td> --}}
+                    <td>{{ $booking->id_number }}</td>
+                    <td> {{ booking_status($booking->status) }}</td>
+                    <td>{{ $booking->created_at->format('Y-m-d') }}</td>
+                    <td> {{ $booking->DOB }}</td>
+                    <td> {{ $booking->phone }}</td>
+                    <td>{{ @$booking->worker->name }}</td>
+                    <td>{{ $booking->order_id }}</td>
+                    <td>{{ $booking->name }}</td>
 
 
                 </tr>
--- resources/views/dashboard/worker/pdf.blade.php
+++ resources/views/dashboard/worker/pdf.blade.php
@@ -1,95 +1,110 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<style>
-    *{
-        direction: rtl
-    }
-table {
-  width:100%;
-}
-table, th, td {
-  border: 1px solid black;
-  border-collapse: collapse;
-}
-th, td {
-  padding: 15px;
-  text-align: left;
-}
-table#t01 tr:nth-child(even) {
-  background-color: #eee;
-}
-table#t01 tr:nth-child(odd) {
- background-color: #fff;
-}
-table#t01 th {
-  background-color: black;
-  color: white;
-}
-.center{
-    text-align: center
-}
-</style>
+    <style>
+        * {
+            direction: rtl
+        }
+
+        table {
+            width: 100%;
+        }
+
+        table,
+        th,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        th,
+        td {
+            padding: 15px;
+            text-align: left;
+        }
+
+        table#t01 tr:nth-child(even) {
+            background-color: #eee;
+        }
+
+        table#t01 tr:nth-child(odd) {
+            background-color: #fff;
+        }
+
+        table#t01 th {
+            background-color: black;
+            color: white;
+        }
+
+        .center {
+            text-align: center
+        }
+    </style>
 </head>
+
 <body>
 
-<h2 class="center">	<img src="https://workers.foryougo.net/uploads/general/PwsoWpkahkzTTiLtkFQRKzwOLPCKs1Uo7BTwjtt3.jpg" width="150" height="100"  /></h2>
-<h2 class="center">العاملات</h2>
-<table id="t01">
-    <tr>
-        
-    
-   
+    <h2 class="center"> <img
+            src="https://workers.foryougo.net/uploads/general/PwsoWpkahkzTTiLtkFQRKzwOLPCKs1Uo7BTwjtt3.jpg" width="150"
+            height="100" /></h2>
+    <h2 class="center">العاملات</h2>
+    <table id="t01">
+        <tr>
 
-        <th style="width: 12%">
-            الديانة
-    </th>
 
-    <th style="width: 15%">
-        اللغات
-    </th>
-    <th style="width: 9%">
-        خبرة
-    </th>
-    <th style="width: 9%">
-        العمر
-    </th>
-    <th style="width: 12%">
-        الحالة
-    </th>
-    <th style="width: 11%">
-        الجنسية
-    </th>
-    <th style="width: 12%">
-        اسم المكتب
-   </th>
-   <th style="width: 10%">
-    اسم العاملة
-</th>
 
-    
 
-  </tr>
-  @foreach ($workers as $worker)
-  <tr>
-      
-      <td style="text-align: right">{{ __($worker->religion) }}</td>
-      <td style="text-align: right"> {{ (get_lang_worker( $worker->language))  }}</td>
-      <td style="text-align: right">{{ $worker->experience }} </td>
-      <td style="text-align: right"> {{ $worker->age }} </td>
-      <td style="text-align: right"> {{ worker_status($worker) }}</td>
-      <td style="text-align: right">{{ $worker->natonality->name }}</td>
-      <td style="text-align: right">{{ $worker->company->name }}</td>
-      <td style="text-align: right">{{ $worker->name }}</td>
+            <th style="width: 12%">
+                الديانة
+            </th>
 
-  </tr>
-@endforeach
- 
- 
-</table>
-<br>
+            <th style="width: 15%">
+                اللغات
+            </th>
+            <th style="width: 9%">
+                خبرة
+            </th>
+            <th style="width: 9%">
+                العمر
+            </th>
+            <th style="width: 12%">
+                الحالة
+            </th>
+            <th style="width: 11%">
+                الجنسية
+            </th>
+            <th style="width: 12%">
+                اسم المكتب
+            </th>
+            <th style="width: 10%">
+                اسم العاملة
+            </th>
+
+
+
+        </tr>
+        @foreach ($workers as $worker)
+            <tr>
+
+                <td style="text-align: right">{{ __($worker->religion) }}</td>
+                <td style="text-align: right"> {{ get_lang_worker($worker->language) }}</td>
+                <td style="text-align: right">{{ $worker->experience }} </td>
+                <td style="text-align: right"> {{ $worker->age }} </td>
+                <td style="text-align: right"> {{ worker_status($worker) }}</td>
+                <td style="text-align: right">{{ $worker->natonality->name }}</td>
+                <td style="text-align: right">{{ $worker->company->name }}</td>
+                <td style="text-align: right">{{ $worker->name }}</td>
+
+            </tr>
+        @endforeach
+
+
+    </table>
+    <br>
 
 
 
 </body>
+
 </html>
